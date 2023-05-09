cd6f8878d0227c47e5adf9ef5f10b9f4ef9f67df
Cabral221 <cabraldiop18@gmail.com>
2023-05-09 10:59:04 +0000

    Ajouter  une annonce de maintenance

--- database/seeders/AnnouncementSeeder.php
+++ database/seeders/AnnouncementSeeder.php
@@ -30,7 +30,7 @@ class AnnouncementSeeder extends Seeder
             Announcement::create([
                 'area' => null,
                 'type' => 'info',
-                'message' => 'This is a <strong>Global</strong> announcement that will show on both the frontend and backend. <em>See <strong>AnnouncementSeeder</strong> for more usage examples.</em>',
+                'message' => 'Ce plateforme est en construction ! Merci de revenir ulterieurement. <em>Voir <strong>MDCSNEST - SG - CI </strong>.</em>',
                 'enabled' => true,
             ]);
 
