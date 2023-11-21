d4915089528a382363b072b8090df8d26c78109e
Cabral221 <cabraldiop18@gmail.com>
2023-11-21 12:58:18 +0000

    Feat : Changer le design des cartes services RH

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -6,42 +6,60 @@
         <div class="col-md-9 text-center">
             <div class="card bg-dark text-white border-0">
                 <img class="card-img" src="{{ asset('img/logo-mdcsnest-h.PNG') }}" alt="Card image">
-              </div>
-
-            <h3 class="title">Bénéficier de nos services en ligne</p>
-
-            <div class="flex flex-wrap flex-center justify-content-center text-white">
-                <a href="{{ route('frontend.imputation.index') }}" class="card bg-primary m-3" style="max-width: 18rem; min-width: 14rem">
-                    <div class="card-body">
-                        <h5 class="card-title">Imputation Budgétaire</h5>
-                    </div>
-                </a>
-                <a href="#" class="card bg-success m-3" style="max-width: 18rem; min-width: 14rem">
-                    <div class="card-body">
-                        <h5 class="card-title">Demande de Congés</h5>
-                    </div>
-                </a>
-                <a href="#" class="card bg-danger m-3" style="max-width: 18rem; min-width: 14rem">
-                    <div class="card-body">
-                        <h5 class="card-title">Ordre de Missions</h5>
-                    </div>
-                </a>
-                <a href="#" class="card bg-warning m-3" style="max-width: 18rem; min-width: 14rem">
-                    <div class="card-body">
-                        <h5 class="card-title">Imputation Budgétaire</h5>
-                    </div>
-                </a>
-                <a href="#" class="card bg-info m-3" style="max-width: 18rem; min-width: 14rem">
-                    <div class="card-body">
-                        <h5 class="card-title">Imputation Budgétaire</h5>
-                    </div>
-                </a>
-                <a href="#" class="card bg-secondary m-3" style="max-width: 18rem; min-width: 14rem">
-                    <div class="card-body">
-                        <h5 class="card-title">Imputation Budgétaire</h5>
-                    </div>
-                </a>
             </div>
+            <hr>
+            <h3 class="title text-primary">Bénéficier de nos services RH en ligne</p>
+            <hr>
+            {{-- Example --}}
+            <div class="py-3">
+                <!-- Row  -->
+                <div class="row">
+                    <!-- Column -->
+                    <div class="col-md-6">
+                        <a href="{{ route('frontend.imputation.index') }}" class="card border-1 card-shadow mb-4 bg-primary">
+                            <div class="card-body d-flex">
+                                <div class="mr-4 align-self-center display-3">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM64 80c0-8.8 7.2-16 16-16h64c8.8 0 16 7.2 16 16s-7.2 16-16 16H80c-8.8 0-16-7.2-16-16zm0 64c0-8.8 7.2-16 16-16h64c8.8 0 16 7.2 16 16s-7.2 16-16 16H80c-8.8 0-16-7.2-16-16zm128 72c8.8 0 16 7.2 16 16v17.3c8.5 1.2 16.7 3.1 24.1 5.1c8.5 2.3 13.6 11 11.3 19.6s-11 13.6-19.6 11.3c-11.1-3-22-5.2-32.1-5.3c-8.4-.1-17.4 1.8-23.6 5.5c-5.7 3.4-8.1 7.3-8.1 12.8c0 3.7 1.3 6.5 7.3 10.1c6.9 4.1 16.6 7.1 29.2 10.9l.5 .1 0 0 0 0c11.3 3.4 25.3 7.6 36.3 14.6c12.1 7.6 22.4 19.7 22.7 38.2c.3 19.3-9.6 33.3-22.9 41.6c-7.7 4.8-16.4 7.6-25.1 9.1V440c0 8.8-7.2 16-16 16s-16-7.2-16-16V422.2c-11.2-2.1-21.7-5.7-30.9-8.9l0 0c-2.1-.7-4.2-1.4-6.2-2.1c-8.4-2.8-12.9-11.9-10.1-20.2s11.9-12.9 20.2-10.1c2.5 .8 4.8 1.6 7.1 2.4l0 0 0 0 0 0c13.6 4.6 24.6 8.4 36.3 8.7c9.1 .3 17.9-1.7 23.7-5.3c5.1-3.2 7.9-7.3 7.8-14c-.1-4.6-1.8-7.8-7.7-11.6c-6.8-4.3-16.5-7.4-29-11.2l-1.6-.5 0 0c-11-3.3-24.3-7.3-34.8-13.7c-12-7.2-22.6-18.9-22.7-37.3c-.1-19.4 10.8-32.8 23.8-40.5c7.5-4.4 15.8-7.2 24.1-8.7V232c0-8.8 7.2-16 16-16z"/></svg>
+                                </div>
+                                <div class="align-self-center text-white">
+                                   <h5>IMPUTATION BUDGETAIRE</h5>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- Column -->
+                    <!-- Column -->
+                    <div class="col-md-6">
+                        <a href="#" class="d-block card border-0 card-shadow mb-4 bg-primary">
+                            <div class="card-body d-flex">
+                                <div class="mr-4 align-self-center display-3">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M208 96a48 48 0 1 0 0-96 48 48 0 1 0 0 96zM123.7 200.5c1-.4 1.9-.8 2.9-1.2l-16.9 63.5c-5.6 21.1-.1 43.6 14.7 59.7l70.7 77.1 22 88.1c4.3 17.1 21.7 27.6 38.8 23.3s27.6-21.7 23.3-38.8l-23-92.1c-1.9-7.8-5.8-14.9-11.2-20.8l-49.5-54 19.3-65.5 9.6 23c4.4 10.6 12.5 19.3 22.8 24.5l26.7 13.3c15.8 7.9 35 1.5 42.9-14.3s1.5-35-14.3-42.9L281 232.7l-15.3-36.8C248.5 154.8 208.3 128 163.7 128c-22.8 0-45.3 4.8-66.1 14l-8 3.5c-32.9 14.6-58.1 42.4-69.4 76.5l-2.6 7.8c-5.6 16.8 3.5 34.9 20.2 40.5s34.9-3.5 40.5-20.2l2.6-7.8c5.7-17.1 18.3-30.9 34.7-38.2l8-3.5zm-30 135.1L68.7 398 9.4 457.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L116.3 441c4.6-4.6 8.2-10.1 10.6-16.1l14.5-36.2-40.7-44.4c-2.5-2.7-4.8-5.6-7-8.6zM550.6 153.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L530.7 224H384c-17.7 0-32 14.3-32 32s14.3 32 32 32H530.7l-25.4 25.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l80-80c12.5-12.5 12.5-32.8 0-45.3l-80-80z"/></svg>
+                                </div>
+                                <div class="align-self-center text-white">
+                                    <h5>DECISION DE CONGE</h5>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- Column -->
+                    <!-- Column -->
+                    <div class="col-md-6">
+                        <a href="#" class="d-block card border-0 card-shadow mb-4 bg-primary">
+                            <div class="card-body d-flex">
+                                <div class="mr-4 align-self-center display-3">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M152.1 38.2c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 113C-2.3 103.6-2.3 88.4 7 79s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zm0 160c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 273c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zM224 96c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H256c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H256c-17.7 0-32-14.3-32-32zM160 416c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H192c-17.7 0-32-14.3-32-32zM48 368a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
+                                </div>
+                                <div class="align-self-center text-white">
+                                    <h5>ORDRE DE MISSION</h5>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- Column -->
+                </div>
+            </div>
+            {{-- End Example --}}
+
         </div><!--col-md-10-->
     </div><!--row-->
 </div><!--container-->
