084fdd2d4ad2108aa93d9029977be7e6e70089dc
Cabral221 <cabraldiop18@gmail.com>
2023-12-14 17:15:17 +0000

    Fix : form group row pour le formulaire de demande d'imputation

--- resources/views/frontend/pages/imputation.blade.php
+++ resources/views/frontend/pages/imputation.blade.php
@@ -22,72 +22,92 @@
                             @method('POST')
                             <fieldset class="fieldset">
                                 <legend>Information de la personne Malade</legend>
-                                <div class="form-group">
-                                    <label for="sick_name">Penom et Nom de la personne malade</label>
-                                    <input type="text" class="form-control @error('sick_name') is-invalid @enderror" id="sick_name" name="sick_name" placeholder="Prénom et NOM de la personne malade">
-                                    @error('sick_name')
-                                    <span class="invalide-feedback">{{ $message }}</span>
-                                    @enderror
+                                <div class="form-group row">
+                                    <label for="sick_name" class="col-sm-4 col-form-label">Penom et Nom de la personne malade</label>
+                                    <div class="col-sm-8">
+                                        <input type="text" class="form-control @error('sick_name') is-invalid @enderror" id="sick_name" name="sick_name" placeholder="Prénom et NOM de la personne malade">
+                                        @error('sick_name')
+                                            <span class="invalide-feedback">{{ $message }}</span>
+                                        @enderror
+                                        <div class="custom-control custom-switch m-1">
+                                            <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                                            <label class="custom-control-label" for="customSwitch1">C'est Moi-même </label>
+                                        </div>
+                                    </div>
                                 </div>
                             </fieldset>
                             <fieldset class="fieldset">
                                 <legend>Informations personnelles de l'agent</legend>
-                                <div class="form-group">
-                                    <label for="first_name">Prénom de l'agent</label>
-                                    <input type="text" class="form-control @error('first_name') is-invalid @enderror" id="first_name" name="first_name" placeholder="Entrer votre prénom" required>
-                                    @error('first_name')
-                                        <span class="invalid-feedback">{{ $message }}</span>
-                                    @enderror
+                                <div class="form-group row">
+                                    <label for="first_name" class="col-sm-4 col-form-label">Prénom</label>
+                                    <div class="col-sm-8">
+                                        <input type="text" class="form-control @error('first_name') is-invalid @enderror" id="first_name" name="first_name" placeholder="Entrer votre prénom" required>
+                                        @error('first_name')
+                                            <span class="invalid-feedback">{{ $message }}</span>
+                                        @enderror
+                                    </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="last_name">Nom de l'agent</label>
-                                    <input type="text" class="form-control @error('last_name') is-invalid @enderror" id="last_name" name="last_name" placeholder="Entrer votre nom" required>
-                                    @error('last_name')
-                                        <span class="invalid-feedback">{{ $message }}</span>
-                                    @enderror
+                                <div class="form-group row">
+                                    <label for="last_name" class="col-sm-4 col-form-label">Nom</label>
+                                    <div class="col-sm-8">
+                                        <input type="text" class="form-control @error('last_name') is-invalid @enderror" id="last_name" name="last_name" placeholder="Entrer votre nom" required>
+                                        @error('last_name')
+                                            <span class="invalid-feedback">{{ $message }}</span>
+                                        @enderror
+                                    </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="phone">Téléphone</label>
-                                    <input type="phone" class="form-control @error('phone') is-invalid @enderror" id="phone" name="phone" placeholder="Ex : 771234567" required>
-                                    @error('phone')
-                                    <span class="invalid-feedback">{{ $message }}</span>
-                                    @enderror
+                                <div class="form-group row">
+                                    <label for="phone" class="col-sm-4 col-form-label">Téléphone</label>
+                                    <div class="col-sm-8">
+                                        <input type="phone" class="form-control @error('phone') is-invalid @enderror" id="phone" name="phone" placeholder="Ex : 771234567" required>
+                                        @error('phone')
+                                            <span class="invalid-feedback">{{ $message }}</span>
+                                        @enderror
+                                    </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="email">Adresse E-mail</label>
-                                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="Ex : abc@abc.com">
-                                    @error('email')
-                                        <span class="invalid-feedback">{{ $message }}</span>
-                                    @enderror
+                                <div class="form-group row">
+                                    <label for="email" class="col-sm-4 col-form-label">Adresse E-mail</label>
+                                    <div class="col-sm-8">
+                                        <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="Ex : abc@abc.com">
+                                        @error('email')
+                                            <span class="invalid-feedback">{{ $message }}</span>
+                                        @enderror
+                                    </div>
                                 </div>
                             </fieldset>
                             <hr>
                             <fieldset>
                                 <legend>Services</legend>
-                                <div class="form-group">
-                                    <label for="matricule">Matricule de solde</label>
-                                    <input type="text" class="form-control @error('registration_number') is-invalid @enderror" id="matricule" name="registration_number" placeholder="Ex : 123456/A">
-                                    @error('registration_number')
-                                        <span class="invalid-feedback">{{ $message }}</span>
-                                    @enderror
+                                <div class="form-group row">
+                                    <label for="matricule" class="col-sm-4 col-form-label">Matricule de solde</label>
+                                    <div class="col-sm-8">
+                                        <input type="text" class="form-control @error('registration_number') is-invalid @enderror" id="matricule" name="registration_number" placeholder="Ex : 123456/A">
+                                        @error('registration_number')
+                                            <span class="invalid-feedback">{{ $message }}</span>
+                                        @enderror
+                                    </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="service">Service</label>
-                                    <input type="text" class="form-control @error('service') is-invalid @enderror" id="service" name="service" placeholder="Ex : Cellule Informatique">
-                                    @error('service')
-                                    <span class="invalid-feedback">{{ $message }}</span>
-                                    @enderror
+                                <div class="form-group row">
+                                    <label for="service" class="col-sm-4 col-form-label">Service</label>
+                                    <div class="col-sm-8">
+                                        <input type="text" class="form-control @error('service') is-invalid @enderror" id="service" name="service" placeholder="Ex : Cellule Informatique">
+                                        @error('service')
+                                            <span class="invalid-feedback">{{ $message }}</span>
+                                        @enderror
+                                    </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="service">Fonction</label>
-                                    <input type="text" class="form-control @error('fonction') is-invalid @enderror" id="fonction" name="fonction" placeholder="Ex : Informaticien">
-                                    @error('fonction')
-                                    <span class="invalid-feedback">{{ $message }}</span>
-                                    @enderror
+                                <div class="form-group row">
+                                    <label for="service" class="col-sm-4 col-form-label">Fonction</label>
+                                    <div class="col-sm-8">
+                                        <input type="text" class="form-control @error('fonction') is-invalid @enderror" id="fonction" name="fonction" placeholder="Ex : Informaticien">
+                                        @error('fonction')
+                                            <span class="invalid-feedback">{{ $message }}</span>
+                                        @enderror
+                                    </div>
                                 </div>
                             </fieldset>
 
-                            <button type="submit" class="btn btn-primary">Soumettre</button>
+                            <button type="submit" class="btn btn-block btn-primary">Soumettre</button>
                         </form>
                     </x-slot>
                 </x-frontend.card>
